f1127c6775129ca87a0456cc0d3872d2ec71345b
garygary85535 <garygary85535@gmail.com>
2026-08-29 23:17:27 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -441,6 +441,55 @@ window.BOOK_DATA = [
     ]
   },
   {
+    "page": "플레인",
+    "status": "2.已完結",
+    "ko": "페이지를 찾을 수 없습니다",
+    "zh": "找不到頁面（含外傳）",
+    "original": "",
+    "translation": "https://docs.google.com/document/d/1Mk8xxMrNmOAUE4k6kM7RJkSESmlT-eIwnjjr63dkSJE/edit?usp=drivesdk",
+    "author": "플레인（Plain）",
+    "translator": "不是蓋瑞",
+    "note": "@新入荷",
+    "date": "2026-08-29",
+    "episodes": [
+      {
+        "label": "EP1",
+        "title": "找不到頁面 1",
+        "url": "https://docs.google.com/document/d/1Mk8xxMrNmOAUE4k6kM7RJkSESmlT-eIwnjjr63dkSJE/edit?usp=drivesdk"
+      },
+      {
+        "label": "EP2",
+        "title": "找不到頁面 2",
+        "url": "https://docs.google.com/document/d/1Sbsj2a3daoo5MsKjoplMUdhTQADrRbwPWKjKMgo_boQ/edit?usp=drivesdk"
+      },
+      {
+        "label": "EP3",
+        "title": "找不到頁面 3",
+        "url": "https://docs.google.com/document/d/1ge5K2FlIAceznBDg8zLKg9CQIxZjr6Hhyk3Nqyi-jaI/edit?usp=drivesdk"
+      },
+      {
+        "label": "EP4",
+        "title": "找不到頁面 4",
+        "url": "https://docs.google.com/document/d/1PINWoJp8XnepVvH-8G-ysqPm_VLZi_KlAG9igOYphrw/edit?usp=drivesdk"
+      },
+      {
+        "label": "EP5",
+        "title": "找不到頁面 5",
+        "url": "https://docs.google.com/document/d/1B0sx9BU0N3pzKnKKEiP37F1mQCx-JWAnQsefDiJk-a8/edit?usp=drivesdk"
+      },
+      {
+        "label": "EP6",
+        "title": "無法找到頁面 6 完結",
+        "url": "https://docs.google.com/document/d/1hcadWMilLQcXg1B9UZxJZa3aFslmVNufcUWsaMP6e7g/edit?usp=drivesdk"
+      },
+      {
+        "label": "外傳",
+        "title": "外傳-已找到頁面",
+        "url": "https://docs.google.com/document/d/1_p9MCmvaLGo2n7QDfVQgITOZTwQqIT5GMikoWHMMjio/edit?usp=drivesdk"
+      }
+    ]
+  },
+  {
     "page": "noname",
     "status": "2.已完結",
     "ko": "구원따윈 필요 없어",
@@ -2378,50 +2427,6 @@ window.BOOK_DATA = [
         "label": "",
         "title": "Off limits_후뿌",
         "url": "https://docs.google.com/document/d/18kg9YXfso3jb20ipVq9iTcZSPr052yPotrt3QFO7VZo/edit?usp=drivesdk"
-      }
-    ]
-  },
-  {
-    "page": "플레인",
-    "status": "2.已完結",
-    "ko": "페이지를 찾을 수 없습니다",
-    "zh": "找不到頁面",
-    "original": "https://www.postype.com/@monochrome-film/post/22545325",
-    "translation": "https://docs.google.com/document/d/1Mk8xxMrNmOAUE4k6kM7RJkSESmlT-eIwnjjr63dkSJE/edit?usp=drivesdk",
-    "author": "플레인（Plain）",
-    "translator": "不是蓋瑞",
-    "note": "",
-    "date": "2026-07-18",
-    "episodes": [
-      {
-        "label": "EP1",
-        "title": "找不到頁面 1",
-        "url": "https://docs.google.com/document/d/1Mk8xxMrNmOAUE4k6kM7RJkSESmlT-eIwnjjr63dkSJE/edit?usp=drivesdk"
-      },
-      {
-        "label": "EP2",
-        "title": "找不到頁面 2",
-        "url": "https://docs.google.com/document/d/1Sbsj2a3daoo5MsKjoplMUdhTQADrRbwPWKjKMgo_boQ/edit?usp=drivesdk"
-      },
-      {
-        "label": "EP3",
-        "title": "找不到頁面 3",
-        "url": "https://docs.google.com/document/d/1ge5K2FlIAceznBDg8zLKg9CQIxZjr6Hhyk3Nqyi-jaI/edit?usp=drivesdk"
-      },
-      {
-        "label": "EP4",
-        "title": "找不到頁面 4",
-        "url": "https://docs.google.com/document/d/1PINWoJp8XnepVvH-8G-ysqPm_VLZi_KlAG9igOYphrw/edit?usp=drivesdk"
-      },
-      {
-        "label": "EP5",
-        "title": "找不到頁面 5",
-        "url": "https://docs.google.com/document/d/1B0sx9BU0N3pzKnKKEiP37F1mQCx-JWAnQsefDiJk-a8/edit?usp=drivesdk"
-      },
-      {
-        "label": "EP6",
-        "title": "無法找到頁面 6 完結",
-        "url": "https://docs.google.com/document/d/1hcadWMilLQcXg1B9UZxJZa3aFslmVNufcUWsaMP6e7g/edit?usp=drivesdk"
       }
     ]
   },
